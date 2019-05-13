a75a21af8b8105a32891db945c2585e140aedf41
zd6515843 <32858918+zd6515843@users.noreply.github.com>
2019-05-13 13:29:01 +0800

    Update Expedia.java

--- SeleniumJavaCucumber/src/tests/Expedia.java
+++ SeleniumJavaCucumber/src/tests/Expedia.java
@@ -1,8 +1,12 @@
 package tests;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,8 +17,9 @@ public class Expedia {
 	String city = "New York, NY";
 	String checkin = "11/01/2019";
 	String checkout = "11/08/2019";
-	String numOfChild= "2";
-	String stars= "star-3";//4 star
+	String numOfChild = "2";
+	String starRating = "3 star";
+	String searchResult = "3";
 	
 	@Test
 	public void hotelReservation() {
@@ -36,16 +41,37 @@ public class Expedia {
 		
 		
 		// 2. Modify the search result page, give criteria
-		driver.findElement(By.id(stars)).click();
-//		driver.findElements(By.cssSelector(".property-class-input.all-grid-nowrap.uitk-grid span")).get(3).click();
+//		driver.findElement(By.cssSelector("input[type=checkbox][aria-label='" + starRating + "']")).click();
+
 		
 		// 3. Analyze the result and make our selection
+		driver.findElement(By.xpath("//section[@class='results']//li[@tabindex='-1'][" + searchResult + "]//a")).click();
 
+		// Switch the window to the popUp
+		ArrayList<String> windows = new ArrayList<String> (driver.getWindowHandles());
+		driver.switchTo().window(windows.get(1));
+		
+		// Print hotel name and star rating
+		String hotelName = driver.findElement(By.cssSelector("section[class='uitk-cell uitk-card-text-segment all-x-padding-three uitk-card-separator-bottom'] h1")).getText();
+		String hotelRating = driver.findElement(By.cssSelector("span[class='reviews-summary__rating-value']")).getText();
+		System.out.println("Hotel : " + hotelName + " Rating : " + hotelRating);
+		
+//		driver.switchTo().window(windows.get(0));
+//		driver.findElement(By.xpath("//section[@class='results']//li[@tabindex='-1'][1]//a")).click();
+		
+		
 		// 4. Book reservation
+		driver.findElements(By.cssSelector("button[data-stid=submit-hotel-reserve]")).get(0).click();
+		
+		// print total price
+		String price = driver.findElement(By.cssSelector("span[data-price-update='total']")).getText();
+		System.out.println("Pirce: " + price);
 		
 		// 5. Fill out contact / billing
 		
 		// 6. Get confirmation
+		String pageTitle = driver.getTitle();
+		Assert.assertTrue(pageTitle.contains("Payment"));
 	}
 	
 	@BeforeMethod
@@ -53,6 +79,8 @@ public class Expedia {
 		driver = utilities.DiverFactory.open(browserType);
 		driver.manage().window().maximize();
 		driver.get("https://www.expedia.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
 	}
 	
 	@AfterMethod
